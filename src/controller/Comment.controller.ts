ef3d43f03471fec7b408b3d3245ee1e409f2fc85
Thiago <thiago.goncalvesgames@gmail.com>
2025-09-21 16:54:42 -0300

    feat: implementar controlador de comentários com métodos CRUD

--- src/controller/Comment.controller.ts
+++ src/controller/Comment.controller.ts
@@ -1,66 +1,62 @@
 import { NextFunction, Request, Response } from "express";
-import { PostService } from "../services/Post.services";
 import Filter from "../interfaces/Filter";
-import filterConfig from "../utils/filterConfig";
 import IController from "../interfaces/IController";
+import filterConfig from "../utils/filterConfig";
+import { CommentService } from "../services/Comment.services";
 
-const postService = new PostService();
+const commentService = new CommentService();
 
-export default class PostController implements IController {
-    async getAll(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const allowedQueryFields: string[] = ["title", "content", "date", "likes", "image"];
-            const filters: Filter = filterConfig(req.query, allowedQueryFields);
-
-            const posts = await postService.getAll(filters);
-            res.status(200).json(posts);
-        } catch (error) {
-            next(error);
-        }
-    }
-
+export class CommentController implements IController {
     async getById(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const id = req.params.id;
             if (!id) {
                 throw new Error("ID não foi informado.");
             }
-            const post = await postService.getById(id);
-            res.status(200).json(post);
+            const comment = await commentService.getById(id);
+            res.status(200).json(comment);
+        } catch (error: any) {
+            next(error);
+        }
+    }
+    async getAll(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const allowedQueryFields: string[] = ["postId", "author", "content", "date", "likes"];
+            const filters: Filter = filterConfig(req.query, allowedQueryFields);
+
+            const comments = await commentService.getAll(filters);
+            res.status(200).json(comments);
         } catch (error) {
             next(error);
         }
     }
-
     async create(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            const post = await postService.create(req.body);
-            res.status(201).json(post);
+            const comment = await commentService.create(req.body);
+            res.status(201).json(comment);
         } catch (error) {
             next(error);
         }
     }
-
     async update(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const id = req.params.id;
             if (!id) {
                 throw new Error("ID não foi informado.");
             }
-            const post = await postService.update(id, req.body);
-            res.status(200).json(post);
+            const comment = await commentService.update(id, req.body);
+            res.status(200).json(comment);
         } catch (error) {
             next(error);
         }
     }
-
     async delete(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const id = req.params.id;
             if (!id) {
                 throw new Error("ID não foi informado.");
             }
-            await postService.delete(id);
+            await commentService.delete(id);
             res.status(204).json();
         } catch (error) {
             next(error);
