15fbbabedee59eb934c0fa1be8ccf6a0b04fd4be
LucasFSouza552 <lucasfelipe552@hotmail.com>
2025-09-21 16:58:45 -0300

    feat: adicionar middleware de autorização e refatorar rotas para incluir validação de acesso

---
+++ src/middleware/authorizationMiddleware.ts
@@ -0,0 +1,42 @@
+import { AccountDTO } from "../dtos/AccountDTO";
+import { ThrowError } from "../errors/ThrowError";
+import { IAccount } from "../models/Account";
+import { AccountService } from "./../services/Account.services";
+import { NextFunction, Response, Request } from "express";
+
+const accountService = new AccountService();
+
+export default function authorizationMiddleware(allowedRoles: string[]) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+
+            const accountId = req.accountId;
+
+            if (!accountId) {
+                throw ThrowError.unauthorized("Usuário não autenticado");
+            }
+
+            const account = await accountService.getById(accountId) as IAccount;
+
+            if (!account) {
+                throw ThrowError.notFound("Usuário não existente");
+            }
+
+            if (!allowedRoles.includes(account.role)) {
+                throw ThrowError.forbidden("Você não tem permissão para acessar este recurso.");
+            }
+
+            req.account = account;
+
+            next();
+
+        } catch (error: any) {
+            if (error instanceof ThrowError) {
+                res.status(error.statusCode).json({ error: error.message });
+            } else {
+                console.error(error);
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        }
+    }
+}
